8f201eba468557df4e6c377117689b8b976b8aee
Patrick <patthenoble0@gmail.com>
2021-05-10 16:54:58 -0400

    Added dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,7 +3,7 @@ import path from 'path'
 
 //const postsDirectory = path.join(process.cwd(), 'posts')
 
-export async function getStaticPaths() {
+/* export async function getStaticPaths() {
   const postsDirectory = path.join(process.cwd(), 'posts')
   const fileNames = await fs.readdir(postsDirectory)
   fileNames.map(fileName => {
@@ -12,7 +12,7 @@ export async function getStaticPaths() {
         id: fileName.replace(/\.md$/, '')
       }
     }
-  })
+  }) 
 
   const paths = fileNames;
   return {
@@ -20,6 +20,7 @@ export async function getStaticPaths() {
     fallback: false
   }
 }
+*/
 
 // posts will be populated at build time by getStaticProps()
 function Blog({ posts }) {
@@ -27,7 +28,7 @@ function Blog({ posts }) {
     <ul>
       {posts.map((post) => (
         <li>
-          <h3>{post.filename}</h3>
+          <h3>{post.name}</h3>
           <p>{post.content}</p>
         </li>
       ))}
@@ -36,35 +37,52 @@ function Blog({ posts }) {
 }
 
 
-/* export async function getStaticPaths() {
+export async function getStaticPaths() {
+  
+  const postsDirectory = path.join(process.cwd(), 'posts')
+  const fileNames = await fs.readdir(postsDirectory)
+  const posts = fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    }
+  }) 
+
   return {
+    paths: posts,
+    fallback: false
+  }
+
+/*   return {
     paths: [
-      { params: { id: '2' } },
-      { params: { id: '1' } }
+      { params: { id: 'test' } },
+      { params: { id: 'pre-rendering' } }
     ],
     fallback: false // See the "fallback" section below
-  };
-} */
+  }; */
+}
 
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
 // direct database queries. See the "Technical details" section.
 export async function getStaticProps() {
-  const postsDirectory = path.join(process.cwd(), 'posts')
+  //const postsDirectory = path.join(process.cwd(), 'posts')
+  const postsDirectory = "posts/"
   const filenames = await fs.readdir(postsDirectory)
 
-  const posts = filenames.map(async (filename) => {
-    const filePath = path.join(postsDirectory, filename)
+  const posts = filenames.map(async (fileName) => {
+    const filePath = path.join(postsDirectory, fileName)
     const fileContents = await fs.readFile(filePath, 'utf8')
 
     // Generally you would parse/transform the contents
     // For example you can transform markdown to HTML here
-
     return {
-      filename,
+      // Set the current element of posts to a post with post.name and post.content
+      name: fileName.replace(/\.md$/, ''), 
       content: fileContents,
     }
-  })
+  }) // End posts Map
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
   return {
---
+++ posts/Mike's-Biography.md
@@ -0,0 +1,6 @@
+---
+title: 'Mike's Biography'
+date: '999'
+---
+
+Hi, my name is **Moike** and this is the story of how I became the world's strongest hero
