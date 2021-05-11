73e24a8ffeb967c948c5f317dfbf9c5dac7b3a44
Patrick <patthenoble0@gmail.com>
2021-05-11 13:37:10 -0400

    Fixed the issue where each blog post had the text of all other blog posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,47 +1,24 @@
 import { promises as fs } from 'fs'
+import fs2 from 'fs'
 import path from 'path'
+import matter from 'gray-matter'
+import html from 'remark-html'
+import remark from 'remark'
 
-//const postsDirectory = path.join(process.cwd(), 'posts')
-
-/* export async function getStaticPaths() {
-  const postsDirectory = path.join(process.cwd(), 'posts')
-  const fileNames = await fs.readdir(postsDirectory)
-  fileNames.map(fileName => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, '')
-      }
-    }
-  }) 
-
-  const paths = fileNames;
-  return {
-    paths,
-    fallback: false
-  }
-}
-*/
-
-// posts will be populated at build time by getStaticProps()
-function Blog({ posts }) {
+// post will be populated at build time by getStaticProps()
+function Blog({ post }) {
   return (
-    <ul>
-      {posts.map((post) => (
-        <li>
-          <h3>{post.name}</h3>
-          <p>{post.content}</p>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h3>{post.name}</h3>
+      <div dangerouslySetInnerHTML={{ __html: post.content}} />
+    </div>
   )
 }
 
 
 export async function getStaticPaths() {
-  
-  const postsDirectory = path.join(process.cwd(), 'posts')
-  const fileNames = await fs.readdir(postsDirectory)
-  const posts = fileNames.map(fileName => {
+  const fileNames = await fs.readdir("posts/")
+  const postNames = fileNames.map(fileName => {
     return {
       params: {
         id: fileName.replace(/\.md$/, '')
@@ -50,44 +27,33 @@ export async function getStaticPaths() {
   }) 
 
   return {
-    paths: posts,
+    paths: postNames,
     fallback: false
   }
-
-/*   return {
-    paths: [
-      { params: { id: 'test' } },
-      { params: { id: 'pre-rendering' } }
-    ],
-    fallback: false // See the "fallback" section below
-  }; */
 }
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries. See the "Technical details" section.
-export async function getStaticProps() {
-  //const postsDirectory = path.join(process.cwd(), 'posts')
-  const postsDirectory = "posts/"
-  const filenames = await fs.readdir(postsDirectory)
+// params is an optional parameter that gets passed in under the hood.
+export async function getStaticProps({params}) {
 
-  const posts = filenames.map(async (fileName) => {
-    const filePath = path.join(postsDirectory, fileName)
-    const fileContents = await fs.readFile(filePath, 'utf8')
+  const fullPath = path.join("posts/", `${params.id}.md`)
+  const fileContents = fs2.readFileSync(fullPath, 'utf8')
 
-    // Generally you would parse/transform the contents
-    // For example you can transform markdown to HTML here
-    return {
-      // Set the current element of posts to a post with post.name and post.content
-      name: fileName.replace(/\.md$/, ''), 
-      content: fileContents,
-    }
-  }) // End posts Map
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents)
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
   return {
     props: {
-      posts: await Promise.all(posts),
+      post:{
+        id: params.id,
+        name: params.id,
+        content: contentHtml,
+      }
     },
   }
 }
--- posts/Mike's-Biography.md
+++ posts/Mike's-Biography.md
@@ -4,3 +4,4 @@ date: '999'
 ---
 
 Hi, my name is **Moike** and this is the story of how I became the world's strongest hero
+<h3>this iz big text m8</h3>
